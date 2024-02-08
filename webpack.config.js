9ef30a6a14addf927b4b63a8752656979e5ad806
Polina <polina.igrushkina@gmail.com>
2024-02-08 22:56:47 +0300

    three.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,6 @@ const htmlPlugins = generateHtmlPlugins("./src/html/views");
 const config = {
     entry: ["./src/js/index.js", "./src/scss/style.scss"],
     output: {
-        path: path.resolve(__dirname, "dist"),
         filename: "./js/bundle.js",
     },
     devtool: "source-map",
