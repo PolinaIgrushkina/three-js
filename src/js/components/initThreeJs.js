fa30e399d712c570c2e20a258260bad8db1144f8
Polina <polina.igrushkina@gmail.com>
2024-02-09 12:33:25 +0300

    three.js

--- src/js/components/initThreeJs.js
+++ src/js/components/initThreeJs.js
@@ -1,38 +1,38 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const init = (className) => {
-  // Размеры для сцены
-	const sizes = {
-		width: window.innerWidth,
-		height: window.innerHeight,
-	};
+// const init = (className) => {
+//   // Размеры для сцены
+// 	const sizes = {
+// 		width: window.innerWidth,
+// 		height: window.innerHeight,
+// 	};
   
-  // Сцена в DOM
-  const canvas = document.querySelector(`.${className}`);
+//   // Сцена в DOM
+//   const canvas = document.querySelector(`.${className}`);
 
-  if (canvas) {
-    // Сцена
-    const scene = new THREE.Scene();
+//   if (canvas) {
+//     // Сцена
+//     const scene = new THREE.Scene();
   
-    // Настройки угла и позиции камерыи (угол обзора и соотношение сторон)
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+//     // Настройки угла и позиции камерыи (угол обзора и соотношение сторон)
+//     const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
   
-    // Добавление камеры на сцену
-    scene.add(camera);
+//     // Добавление камеры на сцену
+//     scene.add(camera);
 
-    // Контроллеры камеры с помощью специального обьекта управления камерой OrbitControls из библиотеки 
-    const controls = new OrbitControls(camera, canvas);
-    // Сглаживание движения
-    controls.enableDamping = true;
+//     // Контроллеры камеры с помощью специального обьекта управления камерой OrbitControls из библиотеки 
+//     const controls = new OrbitControls(camera, canvas);
+//     // Сглаживание движения
+//     controls.enableDamping = true;
 
-    // Вызов рендера (отрисовщик рендерит сцену от лица камеры)
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.render(scene, camera);
+//     // Вызов рендера (отрисовщик рендерит сцену от лица камеры)
+//     const renderer = new THREE.WebGLRenderer({ canvas });
+//     renderer.setSize(sizes.width, sizes.height);
+//     renderer.render(scene, camera);
 
-    return { sizes, scene, canvas, camera, renderer, controls };
-  }
-};
+//     return { sizes, scene, canvas, camera, renderer, controls };
+//   }
+// };
 
-export default init;
+// export default init;
